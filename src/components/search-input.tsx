7e6c76b1cd1c940b786d0b9523fe6802980feb76
Hosein <hosein.abolhasani1998@gmail.com>
2023-07-31 15:45:23 +0330

    feat:insert search terms in url query string

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import { useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import qs from 'query-string';
 import { Search } from 'lucide-react';
 import { Input } from '@/components/ui/input';
+import { useDebounce } from '@/hooks/use-debounce';
 
 export const SearchInput = () => {
   const router = useRouter();
@@ -13,10 +15,36 @@ export const SearchInput = () => {
   const name = searchParams.get('name ');
 
   const [value, setValue] = useState(name || '');
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  function onChange(event: ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value);
+  }
+
+  useEffect(() => {
+    const query = {
+      name: debouncedValue,
+      categoryId,
+    };
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    router.push(url);
+  }, [debouncedValue, router, categoryId]);
   return (
     <div className="relative">
       <Search className="absolute x-4 h-4 top-3 left-4 text-muted-foreground" />
-      <Input placeholder="Search..." className="pl-10 bg-primary/10" />
+      <Input
+        onChange={onChange}
+        value={value}
+        placeholder="Search..."
+        className="pl-10 bg-primary/10"
+      />
     </div>
   );
 };
