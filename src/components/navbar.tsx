b940d73d5309b37aa025a07a8658b276c567b2d3
Hosein <hosein.abolhasani1998@gmail.com>
2023-07-31 12:02:23 +0330

    feat:add theme provider

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,9 +2,10 @@
 
 import { cn } from '@/lib/utils';
 import { UserButton } from '@clerk/nextjs';
-import { Menu } from 'lucide-react';
+import { Menu, Sparkles } from 'lucide-react';
 import { Poppins } from 'next/font/google';
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 const font = Poppins({
   weight: '600',
@@ -28,6 +29,10 @@ export const Navbar = () => {
         </Link>
       </div>
       <div className="flex items-center gap-x-3">
+        <Button size="sm" variant="premium">
+          Upgrade
+          <Sparkles className="w-4 h-4 fill-white text-white ml-2" />
+        </Button>
         <UserButton afterSignOutUrl="/" />
       </div>
     </div>
