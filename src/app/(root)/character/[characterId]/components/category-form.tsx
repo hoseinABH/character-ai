ca1c1c113c081d4375305889e3267d4c8b64df43
Hosein <hosein.abolhasani1998@gmail.com>
2023-08-01 10:49:17 +0330

    feat:add image uploader

--- src/app/(root)/character/[characterId]/components/category-form.tsx
+++ src/app/(root)/character/[characterId]/components/category-form.tsx
@@ -1,15 +1,93 @@
 'use client';
 
 import { Category, Character } from '@prisma/client';
-
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Separator } from '@/components/ui/separator';
+import { ImageUpload } from '@/components/image-upload';
 interface CategoryFormProps {
   initialData: Character | null;
   categories: Category[];
 }
 
+const formSchema = z.object({
+  name: z.string().min(1, { message: 'Name is required.' }),
+  description: z.string().min(1, { message: 'Description is required.' }),
+  instructions: z
+    .string()
+    .min(200, { message: 'Instructions require at least 200 characters.' }),
+  seed: z
+    .string()
+    .min(200, { message: 'Seed require at least 200 characters.' }),
+  src: z.string().min(1, { message: 'Image is required.' }),
+  categoryId: z.string().min(1, { message: 'Category is required.' }),
+});
+
+type FormData = z.infer<typeof formSchema>;
+
 export const CharacterForm = ({
   initialData,
   categories,
 }: CategoryFormProps) => {
-  return <div>Form</div>;
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: '',
+      description: '',
+      instructions: '',
+      seed: '',
+      src: '',
+      categoryId: undefined,
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  async function onSubmit(values: FormData) {
+    console.log(values);
+  }
+
+  return (
+    <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 pb-10"
+        >
+          <div className="space-y-2 w-full">
+            <div>
+              <h3 className="text-lg font-medium">General Information</h3>
+              <p className="text-sm text-muted-foreground">
+                General information about your character
+              </p>
+            </div>
+            <Separator className="bg-primary/10" />
+          </div>
+          <FormField
+            name="src"
+            render={({ field }) => (
+              <FormItem className="flex flex-col items-center justify-center space-y-4 col-span-2">
+                <FormControl>
+                  <ImageUpload
+                    disabled={isLoading}
+                    onChange={field.onChange}
+                    value={field.value}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 };
