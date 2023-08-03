48dd4692a8bfbd050853f2e0da8ac86434cc5283
Hosein <hosein.abolhasani1998@gmail.com>
2023-08-03 19:56:09 +0330

    feat:add chat form

--- src/app/(chat)/(routes)/chat/[chatId]/components/client-chat.tsx
+++ src/app/(chat)/(routes)/chat/[chatId]/components/client-chat.tsx
@@ -1,7 +1,12 @@
 'use client';
-
+import { useCompletion } from 'ai/react';
 import { Character, Message } from '@prisma/client';
 import { ChatHeader } from '@/components/chat-header';
+import { FormEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { ChatMessageProps } from '@/components/chat-message';
+import { ChatMessages } from '@/components/chat-messages';
+import { ChatForm } from '@/components/chat-form';
 
 interface ClientChatProps {
   character: Character & {
@@ -13,9 +18,51 @@ interface ClientChatProps {
 }
 
 export const ClientChat = ({ character }: ClientChatProps) => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<ChatMessageProps[]>(
+    character.messages
+  );
+
+  const { input, isLoading, handleInputChange, handleSubmit, setInput } =
+    useCompletion({
+      api: `/api/chat/${character.id}`,
+      onFinish(_prompt, completion) {
+        const systemMessage: ChatMessageProps = {
+          role: 'system',
+          content: completion,
+        };
+
+        setMessages((current) => [...current, systemMessage]);
+        setInput('');
+
+        router.refresh();
+      },
+    });
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    const userMessage: ChatMessageProps = {
+      role: 'user',
+      content: input,
+    };
+
+    setMessages((current) => [...current, userMessage]);
+
+    handleSubmit(event);
+  };
   return (
     <div className="flex flex-col h-full p-4 space-y-2">
       <ChatHeader character={character} />
+      <ChatMessages
+        character={character}
+        messages={messages}
+        isLoading={isLoading}
+      />
+      <ChatForm
+        isLoading={isLoading}
+        input={input}
+        handleInputChange={handleInputChange}
+        onSubmit={onSubmit}
+      />
     </div>
   );
 };
